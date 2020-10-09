60e4eba37b66fcc113d11ba62036de4477fd83a4
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-09 05:11:01 -0300

    Listando livros por id

--- src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
+++ src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
@@ -2,7 +2,9 @@ package com.crudBiblioteca.Controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudBiblioteca.Biblioteca.Biblioteca;
@@ -21,4 +23,12 @@ public class BibliotecaController {
 	public List<Biblioteca> list() {
 		 return repository.findAll();
 	}
-}
+	
+	@GetMapping(path = {"listId/{id}"})
+	public ResponseEntity<Biblioteca> findById(@PathVariable long id) {
+	   return repository.findById(id)
+			  .map(listar -> ResponseEntity.ok().body(listar))
+			  .orElse(ResponseEntity.notFound().build());
+	}
+	
+ }
