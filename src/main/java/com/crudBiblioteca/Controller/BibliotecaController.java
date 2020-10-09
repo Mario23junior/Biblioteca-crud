94110ad6b1157f1f43bc9f65fc6cf3795e8a208e
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-09 04:24:43 -0300

    Listando todos os dados do banco

--- src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
+++ src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
@@ -1,8 +1,24 @@
 package com.crudBiblioteca.Controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.crudBiblioteca.Biblioteca.Biblioteca;
+import com.crudBiblioteca.Repository.BibliotecaRepository;
 
 @RestController
 public class BibliotecaController {
-
+      
+	private BibliotecaRepository repository;
+	
+	public BibliotecaController(BibliotecaRepository bibliotecaRepository) {
+		 this.repository = bibliotecaRepository;
+ 	}
+	
+	@GetMapping(value = "/list")
+	public List<Biblioteca> list() {
+		 return repository.findAll();
+	}
 }
