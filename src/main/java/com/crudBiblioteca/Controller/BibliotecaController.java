8f9c936f80ee4243eabf74df6ddeea31031b0e2b
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-09 05:52:57 -0300

    Salvando livros no banco de dados

--- src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
+++ src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudBiblioteca.Biblioteca.Biblioteca;
@@ -29,6 +31,11 @@ public class BibliotecaController {
 	   return repository.findById(id)
 			  .map(listar -> ResponseEntity.ok().body(listar))
 			  .orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping(value = "/save")
+	public Biblioteca create(@RequestBody Biblioteca biblioteca) {
+		return repository.save(biblioteca);
 	}
 	
  }
