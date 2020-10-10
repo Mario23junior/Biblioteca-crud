92fbb1e43fedc5d492579a11edb816b206a91d9a
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-10 03:08:22 -0300

    Deletando livro por id

--- src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
+++ src/main/java/com/crudBiblioteca/Controller/BibliotecaController.java
@@ -3,6 +3,7 @@ package com.crudBiblioteca.Controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,15 @@ public class BibliotecaController {
 	@PostMapping(value = "/save")
 	public Biblioteca create(@RequestBody Biblioteca biblioteca) {
 		return repository.save(biblioteca);
+	}
+	
+	@DeleteMapping(path = {"delete/{id}"})
+	public ResponseEntity<Object> delete(@PathVariable long id) {
+		return repository.findById(id)
+				.map(deletarId -> {
+					repository.deleteById(id);
+					return ResponseEntity.ok().build();
+				}).orElse(ResponseEntity.notFound().build());
 	}
 	
  }
